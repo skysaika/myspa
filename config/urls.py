4cd4f0571e5b75d53805e599abbd265f1687f3cb
saysuu <skysaika@yandex.ru>
2024-02-19 10:08:32 +0300

    add class CourseViewSet in views.py, add config/urls to apps

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('users.urls', namespace='users')),  # маршрут до приложения users
+    path('materials/', include('materials.urls', namespace='materials')),  # маршрут до приложения materials
 ]
--- materials/views.py
+++ materials/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from materials.models import Course
+from materials.serializers import CourseSerializer
+
+
+class CourseViewSet(viewsets.ModelViewSet):
+    """Вьюсет курса."""
+    serializer_class = CourseSerializer
+    queryset = Course.objects.all()
