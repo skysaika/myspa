59691a4803c4421ec4409cf051cc22e0b00c4eaf
saysuu <skysaika@yandex.ru>
2024-02-20 09:28:53 +0300

    add LessonSerializer in serializers.py

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,10 +1,17 @@
 from rest_framework import serializers
 
-from materials.models import Course
+from materials.models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
     """Сериализатор курса."""
     class Meta:
         model = Course
+        fields = "__all__"
+
+
+class LessonSerializer(serializers.ModelSerializer):
+    """Сериализатор урока."""
+    class Meta:
+        model = Lesson
         fields = "__all__"
