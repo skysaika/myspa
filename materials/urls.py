438fc2c30042de9f7e53d702847c2f1412cde78d
saysuu <skysaika@yandex.ru>
2024-02-20 11:21:50 +0300

    add CRUD generics in views.py, urls.py in Lesson

--- materials/urls.py
+++ materials/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from materials.apps import MaterialsConfig
 from rest_framework.routers import DefaultRouter
 
-from .views import CourseViewSet, LessonCreateAPIView
+from .views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, LessonUpdateAPIView, \
+    LessonDeleteAPIView
 
 app_name = MaterialsConfig.name
 
@@ -11,7 +12,10 @@ router = DefaultRouter()
 router.register('courses', CourseViewSet, basename='courses')
 
 
-
 urlpatterns = [
     path('lesson/create/', LessonCreateAPIView.as_view(), name='lesson-create'),  # для создания урока
+    path('lesson/', LessonListAPIView.as_view(), name='lesson-list'),  # для создания урока
+    path('lesson/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson-view'),  # для создания урока
+    path('lesson/update/<int:pk>/', LessonUpdateAPIView.as_view(), name='lesson-update'),  # для создания урока
+    path('lesson/delete/<int:pk>/', LessonDeleteAPIView.as_view(), name='lesson-delete'),  # для создания урока
 ] + router.urls
--- materials/views.py
+++ materials/views.py
@@ -10,8 +10,30 @@ class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
 
 
-class LessonCreateAPIView(generics.CreateAPIView):  # поддерживает только POST
+class LessonCreateAPIView(generics.CreateAPIView):  # only POST
     """Создание урока."""
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
-22
+
+
+class LessonListAPIView(generics.ListAPIView):  # only GET
+    """Список уроков."""
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonRetrieveAPIView(generics.RetrieveAPIView):  # only GET
+    """Получение одного урока."""
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonUpdateAPIView(generics.UpdateAPIView):  # PUT and PATCH
+    """Обновление урока."""
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonDeleteAPIView(generics.DestroyAPIView):  # only DELETE
+    """Удаление урока."""
+    queryset = Lesson.objects.all()
