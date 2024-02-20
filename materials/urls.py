695c45aa09bee4adcd15c2bab826f160a5b39454
saysuu <skysaika@yandex.ru>
2024-02-20 10:01:23 +0300

    add LessonCreateAPIView in view and lesson-create in urls

--- materials/urls.py
+++ materials/urls.py
@@ -1,15 +1,17 @@
+from django.urls import path
+
 from materials.apps import MaterialsConfig
 from rest_framework.routers import DefaultRouter
 
-from .views import CourseViewSet
+from .views import CourseViewSet, LessonCreateAPIView
 
 app_name = MaterialsConfig.name
 
 router = DefaultRouter()
 router.register('courses', CourseViewSet, basename='courses')
-# router.register('lessons', LessonViewSet)
+
 
 
 urlpatterns = [
-
+    path('lesson/create/', LessonCreateAPIView.as_view(), name='lesson-create'),  # для создания урока
 ] + router.urls
--- materials/views.py
+++ materials/views.py
@@ -1,10 +1,17 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 
-from materials.models import Course
-from materials.serializers import CourseSerializer
+from materials.models import Course, Lesson
+from materials.serializers import CourseSerializer, LessonSerializer
 
 
 class CourseViewSet(viewsets.ModelViewSet):
     """Вьюсет курса."""
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
+
+
+class LessonCreateAPIView(generics.CreateAPIView):  # поддерживает только POST
+    """Создание урока."""
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+22
